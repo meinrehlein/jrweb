53cc2ebb5a3760c2c04e7dc21caea4d479ffdf48
meinrehlein <155490503+meinrehlein@users.noreply.github.com>
2025-08-16 17:53:11 +0200

    Create auth.js

---
+++ netlify/functions/auth.js
@@ -0,0 +1,7 @@
+const { createHandler } = require("netlify-cms-oauth-provider");
+
+exports.handler = createHandler({
+  provider: "github",
+  clientId: process.env. Ov23lihOsYXvwEifMJnu,
+  clientSecret: process.env.15fc528da1de9e42c817045833c3ccc03759bd68,
+});
