e1a9e2877fc502ee28e323fe568a243d364761d8
meinrehlein <155490503+meinrehlein@users.noreply.github.com>
2025-08-16 18:42:09 +0200

    Delete auth.js

--- netlify/functions/auth.js
+++
@@ -1,7 +0,0 @@
-const { createHandler } = require("netlify-cms-oauth-provider");
-
-exports.handler = createHandler({
-  provider: "github",
-  clientId: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-});
